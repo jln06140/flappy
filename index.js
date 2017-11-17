55bced65a4799dc9eab612a34a74b1660f354197
jln06140 <jeanluc.nourry.lp2@gmail.com>
2017-11-17 10:16:50 +0100

    17/11

--- index.js
+++ index.js
@@ -12,18 +12,26 @@ var sprite = document.getElementById('sprite');
 var spriteStepWidth = 92;
 
 // Le nombre de vignette - 1
-var spriteSteps     = 2;
+var spriteSteps = 2;
 
 // La vignette sur laquelle je commence
-var step            = 0;
+var step = 0;
+
+var scoreJeu = 0;
+
+var pause = false;
+
+var vie = 3;
+
+var perdu = false;
 
 // Fonction appelée par animateScene()
 // Fonction qui permet d'animer. Imaginons dans notre exemple qu'on soit à step = 2
-function animateSprite () {
+function animateSprite() {
 
   // Je met le left de mon élément à : (-2 * 92) + 'px' -> -184px
   // On voit donc la 3e vignette.
-  sprite.style.left= -step * spriteStepWidth + 'px';
+  sprite.style.left = -step * spriteStepWidth + 'px';
 
   // Step commence à 0, lorsqu'il arrive à 2, il revient à 0.
   // if ( 2 == 2 ) -> Pour reprendre l'exemple de la marelle : si ciel == ciel, je retourne à terre.
@@ -45,21 +53,42 @@ var up = false;
 
 // Quand je suis en train d'appuyer sur espace la variable up est true
 document.onkeydown = function (e) {
-  if ( e.keyCode == 32 ) {
+  if (e.keyCode == 32) {
     up = true;
   }
 }
 
 // Quand je lache espace la variable up redevient false
 document.onkeyup = function (e) {
-  if ( e.keyCode == 32 ) {
+  if (e.keyCode == 32) {
     up = false;
   }
 }
 
+document.onkeypress = function (e) {
+  if (e.keyCode == 112 | e.keyCode == 80) {
+    if (pause == false) {
+      pause = true;
+    }
+    else {
+      pause = false;;
+    }
+
+  }
+}
+
+// if (pause) {
+//   window.setInterval(animateScene, 0);
+//   window.setInterval(score, 0);
+// }
+// else {
+//   window.setInterval(animateScene, 80);
+//   window.setInterval(score, 80);
+// }
+
 // Fonction appelée par animateScene()
 // Fonction pour animer le vol
-function animateFly () {
+function animateFly() {
 
   // offsetTop -> valeur en pixel de la position top de mon element html
   var fall = birdy.offsetTop;
@@ -84,31 +113,32 @@ var pipes = document.getElementsByClassName('pipe');
 var pipeWidth = pipes[0].offsetWidth;
 
 //Fonction appelée par animatePipes()
-function updateHeight (upPipe, downPipe) {
+function updateHeight(upPipe, downPipe) {
 
-   //Pourcentage de mon élément qui fera obstacle
-   var obstacle = 70;
-   // J'assigne automatiquement 1/3 d'obstacle en haut. Et 1/3 d'obstacle en bas.
-   var blockObstacle = obstacle / 3;
-   // Pour le troisième 1/3 -> je veux un random entre 0 et son total.
-   var random = Math.floor( Math.random() * blockObstacle ); // random
-   // Ce random je l'ajoute au 1/3 du haut.
-   var upObstacle = blockObstacle + random; // 1/3 + random
-   // Ce qui reste du troisième 1/3 je l'ajoute au 1/3 du bas.
-   var downObstacle = blockObstacle + (blockObstacle - random); // 1/3 + (1/3 - random)
+  //Pourcentage de mon élément qui fera obstacle
+  var obstacle = 70;
+  // J'assigne automatiquement 1/3 d'obstacle en haut. Et 1/3 d'obstacle en bas.
+  var blockObstacle = obstacle / 3;
+  // Pour le troisième 1/3 -> je veux un random entre 0 et son total.
+  var random = Math.floor(Math.random() * blockObstacle); // random
+  // Ce random je l'ajoute au 1/3 du haut.
+  var upObstacle = blockObstacle + random; // 1/3 + random
+  // Ce qui reste du troisième 1/3 je l'ajoute au 1/3 du bas.
+  var downObstacle = blockObstacle + (blockObstacle - random); // 1/3 + (1/3 - random)
 
-   upPipe.style.height = upObstacle + '%';
-   downPipe.style.height = downObstacle + '%';
+  upPipe.style.height = upObstacle + '%';
+  downPipe.style.height = downObstacle + '%';
 
 }
 
+
 // Fonction appelée par animatePipes() pour vérifier pour chaque pipe, si il rentre en collision avec Birdy
-function touchPipe (pipe) {
+function touchPipe(pipe) {
 
   // Je récupère toutes les limites importantes de mon pipe
   var pipeLeft = pipe.offsetLeft;
   var pipeRight = pipe.offsetLeft + pipe.offsetWidth;
-  var pipeUp = pipe.children[0].offsetTop+ pipe.children[0].offsetHeight;
+  var pipeUp = pipe.children[0].offsetTop + pipe.children[0].offsetHeight;
   var pipeDown = pipe.children[1].offsetTop;
 
   // Je récupère toutes les limites importantes de mon birdy
@@ -118,22 +148,41 @@ function touchPipe (pipe) {
   var birdyDown = birdy.offsetHeight + birdy.offsetTop;
 
   // Si jamais le birdy entre en collision avec un des enfants, up ou down, de mon pipe. Je colore cet enfant en rouge.
-  if ( birdyRight > pipeLeft && birdyLeft < pipeRight ) {
-    if (birdyDown > pipeDown ) {
+  if (birdyRight > pipeLeft && birdyLeft < pipeRight) {
+    if (birdyDown > pipeDown) {
       pipe.children[1].style.backgroundColor = 'red';
+      perdu = true;
+      
     }
     if (birdyUp < pipeUp) {
       pipe.children[0].style.backgroundColor = 'red';
+      perdu = true;
     }
+  }
+  if (birdyUp < 0 || birdyDown > (window.innerHeight)) {
+    perdu = true;
+  }
+
+  finPartie();
+    // document.body.innerHTML = "<div id='perdu'>perdu</div>";
+   
+
+}
+
+function finPartie(){
+  if (perdu) {
+    document.body.innerHTML = "<div id='perdu'><p>perdu</p><p>Votre score est : "+scoreJeu+"</div>";
+    clearInterval(animateInterval );
+    clearInterval(incrementeScore);
   }
 }
 
 // Fonction appelée par animateScene() Pour faire défiler chaque pipe vers la gauche.
-function animatePipes () {
+function animatePipes() {
 
   // Je récupère une collection HTML, qui est un Array complexe et la raison pour laquelle je dois "hack" la fonction .forEach()
   // Pour chacune des pipes, j'éxecute la fonction ci-dessous, où pipe prendra la valeur de chaque pipe l'une après l'autre.
-  Array.prototype.forEach.call( pipes, function (pipe) {
+  Array.prototype.forEach.call(pipes, function (pipe) {
 
     // Je récupère le left de mon pipe
     var left = pipe.offsetLeft;
@@ -156,19 +205,26 @@ function animatePipes () {
     }
 
     // Je vérifie si mon pipe entre en collision avec Birdy
-    touchPipe (pipe);
+    touchPipe(pipe);
   });
 
+}
+
+function score() {
+
+  document.getElementById("score").innerHTML = scoreJeu;
+  scoreJeu++;
 }
 
 //--------------------- GLOBAL -----------------//
 
 // Fonction appelée par mon window.setInterval() toutes les 80ms.
-function animateScene () {
+function animateScene() {
   // Cette fonction appelle du coup toutes ces fonctions
   animateSprite();
   animateFly();
   animatePipes();
+  
 }
 
 //-----------------------------------------------------------
@@ -176,4 +232,17 @@ function animateScene () {
 //-----------------------------------------------------------
 
 // window.setInterval(action, ms) -> exécute une action à interval régulier de ms
-var animationInstance = window.setInterval(animateScene, 80);
+
+
+
+var animateInterval  = window.setInterval(animateScene, 80);
+
+var incrementeScore = window.setInterval(score, 80);
+
+
+
+
+
+
+
+
